3f4d021499a4ed8b401982cd3aed34eea4f5afec
sthanasis <sthanasis1994@gmail.com>
2022-12-22 00:17:17 +0200

    chore: storybook, vite, styled-components setup

---
+++ styled/animations.ts
@@ -0,0 +1,8 @@
+import { keyframes } from "styled-components";
+
+export const ripple = keyframes({
+  to: {
+    opacity: 0,
+    transform: "scale(2)",
+  },
+});
--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  mode: "development",
+  root: "./public",
+  esbuild: {
+    include: "./src/**.(ts | tsx)",
+  },
+  server: { open: "./public/index.html" },
 });
