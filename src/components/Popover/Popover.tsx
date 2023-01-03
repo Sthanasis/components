42fd1b8966d3e2453350960cd91e7d64c299117a
sthanasis <sthanasis1994@gmail.com>
2023-01-03 22:51:00 +0200

    fix: popover position below the button

--- src/components/Popover/Popover.tsx
+++ src/components/Popover/Popover.tsx
@@ -1,12 +1,54 @@
 import Button from '../Button';
-import { MouseEvent } from 'react';
+import { MouseEvent, useRef, useState } from 'react';
 
 const Popover = (): JSX.Element => {
+  const popoverRef = useRef<HTMLDivElement>(null);
+  const [visible, setVisible] = useState(false);
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
   const click = (e: MouseEvent<HTMLButtonElement>) => {
-    console.log(e.currentTarget);
+    setVisible(true);
+    const { x, y, height, width, bottom, top } =
+      e.currentTarget.getBoundingClientRect();
+    if (popoverRef.current) {
+      const {
+        x: pX,
+        y: pY,
+        height: pHeight,
+      } = popoverRef.current.getBoundingClientRect();
+      if (pHeight < height + top) {
+        setCoords({ x: x, y: height + top });
+      } else {
+        // position below button
+        setCoords({ x: x, y: height + top });
+      }
+      console.log(pX, pY, x, y);
+    }
   };
 
-  return <Button onClick={click}>click me</Button>;
+  return (
+    <>
+      <Button onClick={click}>click me</Button>
+      <div
+        style={{
+          position: 'absolute',
+          visibility: visible ? 'visible' : 'hidden',
+          zIndex: 10,
+          padding: 10,
+          backgroundColor: 'beige',
+          top: coords.y,
+          left: coords.x,
+        }}
+        ref={popoverRef}
+      >
+        <div>testttttttttttttttttttttttttttttttt</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+      </div>
+    </>
+  );
 };
 
 export default Popover;
