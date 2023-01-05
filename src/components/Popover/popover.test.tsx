8d07632e94ebe6f1fbce507e43bd0aa567b3d106
sthanasis <sthanasis1994@gmail.com>
2023-01-05 19:11:55 +0200

    test: popover test render

--- src/components/Popover/popover.test.tsx
+++ src/components/Popover/popover.test.tsx
@@ -1,1 +1,18 @@
-/* Write the test cases here! */
+import { render } from 'test-utils';
+import Popover from '.';
+
+const setup = () => {
+  const utils = render(
+    <Popover aria-label="popover" visible={true}>
+      Test
+    </Popover>
+  );
+  return utils;
+};
+
+describe('Popover Component', () => {
+  it('renders', () => {
+    const { queryByLabelText } = setup();
+    expect(queryByLabelText('popover')).toBeInTheDocument();
+  });
+});
