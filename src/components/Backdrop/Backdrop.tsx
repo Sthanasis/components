ece227fd7f62a6bd0f3496abc4a7be2a822b9fab
sthanasis <sthanasis1994@gmail.com>
2023-01-05 19:04:36 +0200

    feat: close popover on esc key press

--- src/components/Backdrop/Backdrop.tsx
+++ src/components/Backdrop/Backdrop.tsx
@@ -1,23 +1,31 @@
-import { MouseEvent, ReactNode } from 'react';
+import { ReactNode, KeyboardEvent } from 'react';
 import { IBaseProps } from 'src/types/props';
 import { StyledBackdrop } from './StyledBackdrop';
 
 interface IBackdropProps extends IBaseProps {
   opacity?: string | number;
-  children: ReactNode;
-  onClick: (e: MouseEvent<HTMLDivElement>) => void | Promise<void>;
+  children?: ReactNode;
+  onClose: () => void | Promise<void>;
 }
 
 const Backdrop = ({
   opacity = 0,
   children,
-  onClick,
+  onClose,
+  ...rest
 }: IBackdropProps): JSX.Element => {
+  const handleKeyPress = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (e.code === '27') {
+      onClose();
+    }
+  };
+
   return (
     <StyledBackdrop
+      {...rest}
       opacity={opacity}
-      onClick={onClick}
-      onKeyDown={(e) => console.log(e)}
+      onMouseUp={onClose}
+      onKeyUp={handleKeyPress}
     >
       {children}
     </StyledBackdrop>
--- src/components/Backdrop/backdrop.test.tsx
+++ src/components/Backdrop/backdrop.test.tsx
@@ -1,1 +1,22 @@
-/* Write the test cases here! */
+import { render, fireEvent } from 'test-utils';
+import Backdrop from '.';
+
+const mockClose = jest.fn();
+
+const setup = () => {
+  const utils = render(<Backdrop aria-label="backdrop" onClose={mockClose} />);
+  return utils;
+};
+
+describe('Backdrop Component', () => {
+  it('renders', () => {
+    const { queryByLabelText } = setup();
+    expect(queryByLabelText('backdrop')).toBeInTheDocument();
+  });
+  it('removes the backdrop on esc key', () => {
+    const { queryByLabelText } = setup();
+    const el = queryByLabelText('backdrop');
+    if (el) fireEvent.keyUp(el, { code: '27' });
+    expect(mockClose).toHaveBeenCalled();
+  });
+});
