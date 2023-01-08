462a31113f6014464b1bab6b877411f02fb006f5
sthanasis <sthanasis1994@gmail.com>
2023-01-08 16:48:33 +0200

    test: testing

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -4,6 +4,8 @@ index = 0
 
 json_string=''
 messages = []
+prefixes = ['fix', 'feat']
+update = ''
 for i in range(1, len(sys.argv)):
     if i > 0:
         json_string = json_string + sys.argv[i]
@@ -13,4 +15,12 @@ arr = json.loads(json_string)
 for commit in arr:
     messages.append(commit['message'])
 
-print(messages)
+for message in messages:
+    if ":" in message:
+        prefix = message.split(":")[0]
+        if prefix in prefixes:
+            if prefix == fix: 
+                update = 'minor'
+            else:
+                update = 'major'
+print(update)
