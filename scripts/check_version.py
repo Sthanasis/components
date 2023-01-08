9579da695c4ab7a64642d9457c1cb7127ae6e895
sthanasis <sthanasis1994@gmail.com>
2023-01-08 13:46:02 +0200

    test: testing v5

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -1,8 +1,9 @@
 import sys
+import json
 
 for i in range(1, len(sys.argv)):
     if i > 0:
-        arr = sys.argv[i]
+        arr = json.loads(sys.argv[i])
         for m in arr:
             print(m.message)
             
