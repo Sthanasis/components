2849888e2d85e83e4dbf2507a0086d286590c0fa
sthanasis <sthanasis1994@gmail.com>
2023-01-08 13:26:47 +0200

    test: keep testing

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -2,8 +2,7 @@ import sys
 
 for i in range(1, len(sys.argv)):
     if i > 0:
-        if isinstance(sys.argv[i], list):
-            arr = sys.argv[i]
-            for m in arr:
-                print(m)
+        arr = sys.argv[i]
+        for m in arr:
+            print(m)
             
