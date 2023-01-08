fd4feb78f8fc78ab17508f8dd971bf4045337648
sthanasis <sthanasis1994@gmail.com>
2023-01-08 15:00:29 +0200

    test

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -1,11 +1,8 @@
 import sys
 import json
-print( len(sys.argv))
-# for i in range(1, len(sys.argv)):
-#     if i > 0:
-#         arr = sys.argv[i]
-#         # print(arr)
-#         print(arr)
-#         # for e in tolist:
-#         #     print(e)
-            
+arr = []
+for i in range(1, len(sys.argv)):
+    if i > 0:
+        arr.append(sys.argv[i])
+
+print(arr)
