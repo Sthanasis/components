bf8105dadbeb497c92bf096d5327a6649221cde5
sthanasis <sthanasis1994@gmail.com>
2023-01-08 13:51:17 +0200

    test: testing v8

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -1,8 +1,10 @@
 import sys
+import json
 
 for i in range(1, len(sys.argv)):
     if i > 0:
         arr = sys.argv[i]
-        for m in arr:
-            print(m.message)
+        for e in arr:
+            event = json.dumb(e)
+            print(event.message)
             
