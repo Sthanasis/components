d286bd734b595c6eceb42abe094ea9e568f88187
sthanasis <sthanasis1994@gmail.com>
2023-01-08 16:24:09 +0200

    test

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -2,14 +2,16 @@ import sys
 import json
 index = 0
 arr=[]
+json_string=''
 messages = []
 for i in range(1, len(sys.argv)):
     if i > 0:
-        arr.append(sys.argv[i])
+        json_string = json_string + sys.argv[i]
+
 for i in range(len(arr)):
     print(arr[i])
     if arr[i] == 'message':
         index = i + 2
     if i == index: 
         messages.append(arr[i])
-print(messages)
+print(json_string)
