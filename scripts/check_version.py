b03624c3ea18436cf65ce4895cd9920db9cdfcf5
sthanasis <sthanasis1994@gmail.com>
2023-01-08 15:03:48 +0200

    test

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -1,8 +1,12 @@
 import sys
 import json
-arr = []
+index = 0
+arr=[]
 for i in range(1, len(sys.argv)):
     if i > 0:
+        continue
+    if sys.argv[i] == 'message:':
+        index = i
+    if i == index + 1:
         arr.append(sys.argv[i])
-
 print(arr)
