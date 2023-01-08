ecb609684091e0688703cc788b972e7d24b5e99c
sthanasis <sthanasis1994@gmail.com>
2023-01-08 15:16:59 +0200

    test

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -4,9 +4,5 @@ index = 0
 arr=[]
 for i in range(1, len(sys.argv)):
     if i > 0:
-        if sys.argv[i] == "message":
-            index = i
-            print(index)
-        if index > 0 and i == index + 1:
-            arr.append(sys.argv[i])
-# print(arr)
+        arr.append(sys.argv[i])
+print(arr)
