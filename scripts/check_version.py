8d3866fef5b398c9b4b5870cdcdfb7372961bf4f
sthanasis <sthanasis1994@gmail.com>
2023-01-08 16:29:57 +0200

    test

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -10,10 +10,7 @@ for i in range(1, len(sys.argv)):
 
 arr = json.loads(json_string)
 
-for i in range(len(arr)):
-    print(arr[i])
-    if arr[i] == 'message':
-        index = i + 2
-    if i == index: 
-        messages.append(arr[i])
+for commit in arr:
+    print(commit.message)
+
 print(type(arr))
