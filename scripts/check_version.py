571a62f2376a38c7ddfd792146855a23a31cd484
sthanasis <sthanasis1994@gmail.com>
2023-01-08 14:56:54 +0200

    test

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -5,7 +5,7 @@ for i in range(1, len(sys.argv)):
     if i > 0:
         arr = sys.argv[i]
         # print(arr)
-        print(arr)
+        print(type(arr))
         # for e in tolist:
         #     print(e)
             
