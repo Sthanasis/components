6b71f65d7bf32a8b7fde8b96b5e96a19a827283d
sthanasis <sthanasis1994@gmail.com>
2023-01-08 15:08:13 +0200

    test

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -7,6 +7,6 @@ for i in range(1, len(sys.argv)):
         continue
     if sys.argv[i] == 'message:':
         index = i
-    if i == index + 1:
-        arr.append(sys.argv[i])
+    if index != 0 and i == index + 1:
+        arr.append(sys.argv[i].split(',')[0])
 print(arr)
