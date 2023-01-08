70ffc727d68e52bb35cc9814a6a043f9ec3b946a
sthanasis <sthanasis1994@gmail.com>
2023-01-08 14:07:38 +0200

    test

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -3,7 +3,7 @@ import json
 for i in range(1, len(sys.argv)):
     if i > 0:
         arr = sys.argv[i]
-        tolist = json.load(arr, 'cls')
+        tolist = json.load(arr)
         print(tolist)
         # for e in tolist:
         #     print(e)
