b189d8f8d685579995911995749e2758e2ee962f
sthanasis <sthanasis1994@gmail.com>
2023-01-10 11:43:34 +0200

    fix: test

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -1,11 +1,23 @@
 import sys
+import os
 import json
-index = 0
 
+def get_current_version():
+    path = os.getcwd()
+    json_file = open(path + "/package.json")
+    pckg_json = json.load(json_file)
+    version = pckg_json['version']
+    print(version)
+    json_file.close()
+    return version
+
+version = get_current_version()
+
+index = 0
 json_string=''
 messages = []
 prefixes = ['breaking', 'fix', 'feat']
-update = ''
+
 for i in range(1, len(sys.argv)):
     if i > 0:
         json_string = json_string + sys.argv[i]
@@ -20,9 +32,12 @@ for message in messages:
         prefix = message.split(":")[0]
         if prefix in prefixes:
             if prefix == 'fix' and (update != 'major' or update != 'minor'): 
-                update = 'patch'
+                update = int(version.split('.')[2]) + 1
+                version = version.split('.')[0] + version.split('.')[1] + update
             elif prefix == 'feat' and update != 'major':
-                update = 'minor'
+                update = int(version.split('.')[1]) + 1
+                version = version.split('.')[0] + update + version.split('.')[2] 
             else: 
-                update = 'major'
-print(update)
+                update = int(version.split('.')[0]) + 1
+                version = update + version.split('.')[1] + version.split('.')[2] 
+print(version)
