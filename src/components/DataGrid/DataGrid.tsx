33db76b77091a41ff7e0ef856f266d39d2bd0808
sthanasis <sthanasis1994@gmail.com>
2023-01-29 19:48:51 +0200

    feat: implement virtual scrolling

--- src/components/DataGrid/DataGrid.tsx
+++ src/components/DataGrid/DataGrid.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react';
 import DatagridProvider, {
   IDatagridContext,
   useDatagrid,
@@ -5,22 +6,59 @@ import DatagridProvider, {
 import DataColumns from './DataColumns/DataColumns';
 import DataRow from './DataRow/DataRow';
 import { Table, TableContainer, Tbody, Thead } from './StyledDataGrid';
+import { ROW_HEIGHT } from './utilities/constants';
 
 const Grid = (): JSX.Element => {
+  const tableRef = useRef<HTMLDivElement>(null);
+
   const { columns, rows, height, width } = useDatagrid();
+  const [scrollTop, setScrollTop] = useState(0);
+
+  const start = Math.max(0, Math.floor(scrollTop / ROW_HEIGHT) - 20);
+  const visibleNodeCount = Math.min(
+    rows.length - start,
+    Math.ceil(500 / ROW_HEIGHT) + 2 * 20
+  );
+  const offsetY = start * ROW_HEIGHT;
+
+  const handleScroll = () => {
+    requestAnimationFrame(() => {
+      if (tableRef.current) {
+        setScrollTop(tableRef.current?.scrollTop);
+      }
+    });
+  };
 
   return (
-    <TableContainer height={height} width={width}>
-      <Table>
-        <Thead>
-          <DataColumns columns={columns} />
-        </Thead>
-        <Tbody>
-          {rows.map((row) => (
-            <DataRow key={row.id} row={row} />
-          ))}
-        </Tbody>
-      </Table>
+    <TableContainer
+      height={height}
+      width={width}
+      ref={tableRef}
+      onScroll={handleScroll}
+    >
+      <div
+        style={{
+          height: (rows.length + 2) * ROW_HEIGHT,
+          overflow: 'hidden',
+          position: 'relative',
+        }}
+      >
+        <div style={{ transform: `translateY(${offsetY}px)` }}>
+          <Table>
+            <Thead>
+              <DataColumns columns={columns} />
+            </Thead>
+            <Tbody>
+              {new Array(visibleNodeCount).fill(null).map((_, index) => (
+                <DataRow
+                  key={rows[start + index].id}
+                  row={rows[start + index]}
+                />
+              ))}
+            </Tbody>
+          </Table>
+        </div>
+      </div>
     </TableContainer>
   );
 };
