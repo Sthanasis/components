2e5f8501368a74439e1a5488f46d4d6cbfd77ccb
sthanasis <sthanasis1994@gmail.com>
2023-02-01 16:08:03 +0200

    style: code cleanup

--- src/components/DataGrid/GridBody/RowList/RowList.tsx
+++ src/components/DataGrid/GridBody/RowList/RowList.tsx
@@ -1,42 +1,13 @@
-import { memo, useMemo } from 'react';
+import { memo } from 'react';
 import { useDatagrid } from 'src/context/datagrid';
-import { RENDER_AHEAD, ROW_HEIGHT } from '../../utilities/constants';
 import DataRow from '../DataRow/DataRow';
 
-interface IRowListProps {
-  start?: number;
-}
-
-const RowList = ({ start }: IRowListProps): JSX.Element => {
-  const { rows, height } = useDatagrid();
-  const totalRows = rows.length;
-  const visibleNodeCount = useMemo(
-    () =>
-      start &&
-      Math.min(
-        rows.length - start,
-        Math.ceil(height / ROW_HEIGHT) + RENDER_AHEAD
-      ),
-    [start, rows]
-  );
-  if (start) {
-    console.log('rerender');
-    return (
-      <>
-        {new Array(visibleNodeCount).fill(null).map((_, index) => (
-          <DataRow
-            key={rows[start + index].id}
-            row={rows[start + index]}
-            noBorder={start + index + 1 === totalRows}
-          />
-        ))}
-      </>
-    );
-  }
+const RowList = (): JSX.Element => {
+  const { rows } = useDatagrid();
   return (
     <>
       {rows.map((row, index) => (
-        <DataRow key={row.id} row={row} noBorder={index + 1 === totalRows} />
+        <DataRow key={row.id} row={row} noBorder={index + 1 === rows.length} />
       ))}
     </>
   );
