3258535883fad818013f26aa2530cbf2c7540090
sthanasis <sthanasis1994@gmail.com>
2023-02-21 16:27:54 +0200

    ci: storybook docker file

--- server.cjs
+++ server.cjs
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const distPath = path.resolve(__dirname, './storybook-static');
 const PORT = 8000;
+const distPath = path.resolve(__dirname, './storybook-static');
 
 app.use(express.static(distPath));
 app.get('*', (req, res) => {
   res.status(200).sendFile(path.resolve(distPath, 'index.html'));
 });
 
-app.listen(PORT, () => console.log('listening on ' + PORT));
+app.listen(PORT, () => console.log('listening on port: ' + PORT));
